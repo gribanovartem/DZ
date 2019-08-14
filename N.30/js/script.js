0810c5a10c89a4a01779e1cd41e6023c162fdd30
gribanovartem <gribanovartem22@gmail.com>
2019-08-14 23:34:55 +0300

    2 commit

--- N.30/js/script.js
+++ N.30/js/script.js
@@ -1,33 +1,44 @@
 
 let allImg = document.getElementsByTagName('img');
+var dragImg;
 for(let i=0; i<allImg.length; i++) {
     allImg[i].style.position = 'absolute';
     allImg[i].addEventListener('mousedown', mousedown, false);
     allImg[i].addEventListener('mousemove', mousemove, false);
     allImg[i].addEventListener('mouseup', mouseup, false);
 }
+var clickX;
+var clickY;
+var index = 0;
 function mousedown(EO) {
-    console.log(EO.currentTarget);
-    let imgPos = getElementPos(EO.currentTarget);
-    dragObject  = this;
+    EO=EO||window.event;
+    EO.preventDefault();
+    let imgPos = getElementPos(this);
+    dragImg  = this;
 
-    var clickX=Math.round(EO.pageX-imgPos.left);
-    var clickY=Math.round(EO.pageY-imgPos.top);
-    console.log(clickX);
-    console.log(clickY);
+    clickX=Math.round(EO.pageX-imgPos.left);
+    clickY=Math.round(EO.pageY-imgPos.top);
+    index++;
+    dragImg.style.zIndex = index;
 }
 function mousemove(EO) {
-    let imgPos = getElementPos(EO.currentTarget);
-    var clickX=Math.round(EO.pageX-imgPos.left);
-    var clickY=Math.round(EO.pageY-imgPos.top);
+    EO=EO||window.event;
+    EO.preventDefault();
+    if(dragImg) {
+        dragImg.style.cursor = 'move';
+        let imgPos1 = getElementPos(dragImg);
+        var clickX1=EO.pageX-imgPos1.left;
+        var clickY1=EO.pageY-imgPos1.top;
+        dragImg.style.top = clickY1+imgPos1.top-clickY+'px';
+        dragImg.style.left = clickX1+imgPos1.left-clickX+'px';
+    }
 }
 function mouseup(EO) {
-    console.log(EO.currentTarget);
-    let imgPos = getElementPos(EO.currentTarget);
-    var clickX=Math.round(EO.pageX-imgPos.left);
-    var clickY=Math.round(EO.pageY-imgPos.top);
-    console.log(clickX);
-    console.log(clickY);
+    EO=EO||window.event;
+    EO.preventDefault();
+    dragImg.style.cursor = 'default';
+    dragImg = null;
+    
 }
 function getElementPos(elem) {
     var bbox=elem.getBoundingClientRect();
