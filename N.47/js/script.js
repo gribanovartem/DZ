4234bccaad314f25e26696cbbaf891665715e2ee
gribanovartem <gribanovartem22@gmail.com>
2019-09-12 20:04:12 +0300

    2 commit

--- N.47/js/script.js
+++ N.47/js/script.js
@@ -24,7 +24,7 @@ function setTennis() {
     SVGElem.appendChild(racketLeft);
 
     let racketRight = document.createElementNS("http://www.w3.org/2000/svg",'rect');
-    racketLeft.setAttribute("id","racketRight");
+    racketRight.setAttribute("id","racketRight");
     racketRight.setAttribute("fill","#191497");
     racketRight.setAttribute("x",tableWidth-tableWidth/60);
     racketRight.setAttribute("y",tableHeight/2-tableHeight/4/2);
@@ -94,6 +94,87 @@ function setTennis() {
             racketRight1.speedY = 0;
         }
     }
+
+    let start = document.getElementById('start');
+    start.addEventListener('click', tick, false);
+    let speedX1 = [4,-4];
+    let speedY1 = [2,3,4,-2,-3,-4,];
+    
+    var ballH={
+        posX : tableWidth/2,
+        posY : tableHeight/2,
+        speedX : speedX1[Math.floor(Math.random()*speedX1.length)],
+        speedY : speedY1[Math.floor(Math.random()*speedY1.length)],
+        width : tableWidth/20,
+        height: tableWidth/20,
+    
+        update : function() {
+            var ballElem=document.getElementById('ball');
+            ballElem.setAttribute("cx", this.posX);
+            ballElem.setAttribute("cy", this.posY);
+        }
+    };
+    let score = document.getElementById('score');
+    function tick() {
+        ballH.update();
+        ballH.posX+=ballH.speedX;
+        ballH.posY+=ballH.speedY;
+
+        if(ballH.posX+ballH.width/2>tableWidth-tableWidth/60 && ballH.posY>racketRight1.posY && ballH.posY<racketRight1.posY+tableHeight/4) {
+            ballH.speedX = -ballH.speedX;
+            ballH.posX = tableWidth-tableWidth/60-ballH.width/2;
+        }
+        if(ballH.posX-ballH.width/2<tableWidth/60 && ballH.posY>racketLeft1.posY && ballH.posY<racketLeft1.posY+tableHeight/4) {
+            ballH.speedX = -ballH.speedX;
+            ballH.posX = tableWidth/60+ballH.width/2;
+        }
+        if (ballH.posX+ballH.width/2>tableWidth) {
+            ballH.speedX = speedX1[Math.floor(Math.random()*speedX1.length)];
+            ballH.speedY = speedY1[Math.floor(Math.random()*speedY1.length)];
+            ballH.posX = tableWidth/2 - tableWidth/20/2;
+            ballH.posY = tableHeight/2 - tableWidth/20/2;
+            player1++;
+            score.textContent = player1+':'+player2;
+            return;
+        }
+        if (ballH.posX<ballH.width/2) {
+            ballH.speedX = speedX1[Math.floor(Math.random()*speedX1.length)];
+            ballH.speedY = speedY1[Math.floor(Math.random()*speedY1.length)];
+            ballH.posX = tableWidth/2 - tableWidth/20/2;
+            ballH.posY = tableHeight/2 - tableWidth/20/2;
+            player2++;
+            score.textContent = player1+':'+player2;
+            return;
+        }
+        if (ballH.posY+ballH.height/2>tableHeight) {
+            ballH.speedY=-ballH.speedY;
+            ballH.posY=tableHeight-ballH.height/2;
+        }
+        if (ballH.posY<ballH.width/2) {
+            ballH.speedY=-ballH.speedY;
+            ballH.posY=ballH.width/2;
+        }
+        racketLeft1.posY += racketLeft1.speedY;
+        racketRight1.posY += racketRight1.speedY;
+        if(racketLeft1.posY<0) {
+            racketLeft1.posY = 0;
+        }
+        if(racketLeft1.posY>tableHeight-tableHeight/4) {
+            racketLeft1.posY = tableHeight-tableHeight/4;
+        }
+        if(racketRight1.posY<0) {
+            racketRight1.posY = 0;
+        }
+        if(racketRight1.posY>tableHeight-tableHeight/4) {
+            racketRight1.posY = tableHeight-tableHeight/4;
+        }
+        
+        racketLeft1.update();
+        racketRight1.update();
+        ballH.update();
+        requestAnimationFrame(tick);
+    }
+    
 
     
 }
